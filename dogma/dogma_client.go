0524e5fa1950001cb12315646992022bd68e942b
Manabu Matsuzaki <matsumana@gmail.com>
2019-10-18 04:37:30 +0900

    Polish

--- dogma/dogma_client.go
+++ dogma/dogma_client.go
@@ -6,41 +6,9 @@ import (
 	"fmt"
 )
 
-// var dogmaFile *CentralDogmaFile
-
 type configFromCentralDogma struct {
 	Greeting string `json:"greeting"`
 }
-
-// // NewClient new client
-// func NewClient() {
-// 	dogmaFile = &CentralDogmaFile{
-// 		Token:      "anonymous", // In Central Dogma Docker image, authentication is disabled so need to use anonymous token.
-// 		BaseURL:    "http://localhost:36462",
-// 		Project:    "fukuoka-go",
-// 		Repo:       "demo",
-// 		Path:       "/config.json",
-// 		TimeoutSec: 30,
-// 	}
-// }
-
-// // Fetch fetch from Central Dogma
-// func Fetch() (string, error) {
-// 	data, err := dogmaFile.Fetch(context.Background())
-// 	if err != nil {
-// 		return "", err
-// 	}
-
-// 	jsonBytes := ([]byte)(data)
-// 	config := new(configFromCentralDogma)
-
-// 	if err := json.Unmarshal(jsonBytes, config); err != nil {
-// 		return "", err
-// 	}
-// 	fmt.Printf("Fetched from Central Dogma: %s\n", config.Greeting)
-
-// 	return config.Greeting, nil
-// }
 
 // Watch Central Dogma file
 func Watch(callback func(string)) error {
--- main.go
+++ main.go
@@ -10,13 +10,6 @@ import (
 var greeting string
 
 func main() {
-	// dogma.NewClient()
-	// fetched, err := dogma.Fetch()
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// greeting = fetched
-
 	err := dogma.Watch(func(data string) {
 		greeting = data
 	})
